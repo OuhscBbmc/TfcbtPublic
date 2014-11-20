f2e6ef309788a98ca1cad129b243e3b1be82eb96
Will Beasley <wibeasley@hotmail.com>
2014-11-19 23:02:25 -0600

    Trying to add debugging version to ShinyApps

--- Shiny/TherapistProgress/Run.R
+++ Shiny/TherapistProgress/Run.R
@@ -3,12 +3,12 @@ rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run. This is 
 library(shiny)
 # server <- (Sys.info()["nodename"] == "Lucky1304")
 # server <- FALSE
-shiny::runApp('./Shiny/Therapist', launch.browser=TRUE, port=6149)
+shiny::runApp('./Shiny/TherapistProgress', launch.browser=TRUE, port=6149)
 # To stop the Shiny server, click the red stop sign in the 'Console window.
 
 # Deploy the **master** branch shinyapps.io if the change is stable.  See https://github.com/rstudio/shinyapps/blob/master/guide/guide.md
 # shinyapps::deployApp(account="melinae")
-# shinyapps::deployApp(account="wibeasley")
+shinyapps::deployApp(account="wibeasley", appDir='./Shiny/TherapistProgress')
 # devtools::install_github("rstudio/shinyapps")
 # shinyapps::showLogs(account="wibeasley")
 # shinyapps::taskLog(taskId=17795, account="wibeasley")
